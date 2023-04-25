515487480206c794637f78d655dfeb31358f2567
Mustafa Panjiwala <56435815+mustafapanjiwala@users.noreply.github.com>
2023-04-26 00:02:45 +0530

    second commit

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -11,26 +11,20 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import MailList from "../../components/mailList/mailList";
 import Footer from "../../components/footer/Footer";
+import { useLocation } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 const Hotel = () => {
+  const location = useLocation();
+  const id = location.pathname.split("/")[2];
+
   const [slideNumber, setSlideNumber] = useState(0);
   const [open, setOpen] = useState(false);
 
-  const handleOpen = (i) => {
-    setOpen(true);
-    setSlideNumber(i);
-  };
+  const { data, error, loading } = useFetch(`/hotels/find/${id}`);
 
-  const handleMove = (dir) => {
-    let newDirection;
-    if (dir === "l") {
-      newDirection = slideNumber === 0 ? 5 : slideNumber - 1;
-    } else if (dir === "r") {
-      newDirection = slideNumber === 5 ? 0 : slideNumber + 1;
-    }
+  console.log(data);
 
-    setSlideNumber(newDirection);
-  };
   const photos = [
     {
       src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1",
@@ -51,93 +45,108 @@ const Hotel = () => {
       src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707389.jpg?k=52156673f9eb6d5d99d3eed9386491a0465ce6f3b995f005ac71abc192dd5827&o=&hp=1",
     },
   ];
+
+  const handleOpen = (i) => {
+    setOpen(true);
+    setSlideNumber(i);
+  };
+
+  const handleMove = (dir) => {
+    let newDirection;
+    if (dir === "l") {
+      newDirection = slideNumber === 0 ? 5 : slideNumber - 1;
+    } else if (dir === "r") {
+      newDirection = slideNumber === 5 ? 0 : slideNumber + 1;
+    }
+
+    setSlideNumber(newDirection);
+  };
   return (
     <div>
       <Navbar />
       <Header type="list" />
-      <div className="hotelContainer">
-        {open && (
-          <div className="slider">
-            <FontAwesomeIcon
-              icon={faCircleXmark}
-              onClick={() => setOpen(false)}
-              className="close"
-            />
-            <FontAwesomeIcon
-              icon={faCircleArrowLeft}
-              className="arrow"
-              onClick={() => handleMove("l")}
-            />
+      {loading ? (
+        "Loading your data"
+      ) : (
+        <>
+          <div className="hotelContainer">
+            {open && (
+              <div className="slider">
+                <FontAwesomeIcon
+                  icon={faCircleXmark}
+                  onClick={() => setOpen(false)}
+                  className="close"
+                />
+                <FontAwesomeIcon
+                  icon={faCircleArrowLeft}
+                  className="arrow"
+                  onClick={() => handleMove("l")}
+                />
 
-            <div className="sliderWrapper">
-              <img src={photos[slideNumber].src} alt="" className="sliderImg" />
-            </div>
+                <div className="sliderWrapper">
+                  <img
+                    src={data?.photos[slideNumber]}
+                    alt=""
+                    className="sliderImg"
+                  />
+                </div>
 
-            <FontAwesomeIcon
-              icon={faCircleArrowRight}
-              className="arrow"
-              onClick={() => handleMove("r")}
-            />
-          </div>
-        )}
-        <div className="hotelWrapper">
-          <button className="bookNow">Reserve or Book Now</button>
-          <h1 className="hotelTitle">Grand Hotel</h1>
-          <div className="hotelAddress">
-            <FontAwesomeIcon icon={faLocationDot} />
-            <span>Elton St 125 New York</span>
-          </div>
-          <span className="hotelDistance">
-            Excellent location – 500m from center
-          </span>
-          <span className="hotelPriceHighlight">
-            Book a stay over $114 at this property and get a free airport taxi
-          </span>
-          <div className="hotelImages">
-            {photos.map((photo, i) => (
-              <div className="hotelImgWrapper">
-                <img
-                  src={photo.src}
-                  onClick={() => handleOpen(i)}
-                  alt=""
-                  className="hotelImg"
+                <FontAwesomeIcon
+                  icon={faCircleArrowRight}
+                  className="arrow"
+                  onClick={() => handleMove("r")}
                 />
               </div>
-            ))}
-          </div>
-          <div className="hotelDetails">
-            <div className="hotelDetailsText">
-              <h1 className="hotelTitle">Stay in the heart of City</h1>
-              <p className="hotelDesc">
-                Located a 5-minute walk from St. Florian's Gate in Krakow, Tower
-                Street Apartments has accommodations with air conditioning and
-                free WiFi. The units come with hardwood floors and feature a
-                fully equipped kitchenette with a microwave, a flat-screen TV,
-                and a private bathroom with shower and a hairdryer. A fridge is
-                also offered, as well as an electric tea pot and a coffee
-                machine. Popular points of interest near the apartment include
-                Cloth Hall, Main Market Square and Town Hall Tower. The nearest
-                airport is John Paul II International Kraków–Balice, 16.1 km
-                from Tower Street Apartments, and the property offers a paid
-                airport shuttle service.
-              </p>
-            </div>
-            <div className="hotelDetailsPrice">
-              <h1>Perfect for a 9-night stay!</h1>
-              <span>
-                Located in the real heart of Krakow, this property has an
-                excellent location score of 9.8!
+            )}
+            <div className="hotelWrapper">
+              <button className="bookNow">Reserve or Book Now</button>
+              <h1 className="hotelTitle">{data.name} </h1>
+              <div className="hotelAddress">
+                <FontAwesomeIcon icon={faLocationDot} />
+                <span>{data.address}</span>
+              </div>
+              <span className="hotelDistance">
+                Excellent location – {data.distance}m from center
               </span>
-              <h2>
-                <b>$945</b> (9 nights)
-              </h2>
-              <button>Reserve or Book Now!</button>
+              <span className="hotelPriceHighlight">
+                Book a stay over ${data.cheapestPrice} at this property and get
+                a free airport taxi
+              </span>
+              <div className="hotelImages">
+                {data.photos?.map((photo, i) => (
+                  <div className="hotelImgWrapper">
+                    <img
+                      src={photo}
+                      onClick={() => handleOpen(i)}
+                      alt=""
+                      className="hotelImg"
+                    />
+                  </div>
+                ))}
+              </div>
+              <div className="hotelDetails">
+                <div className="hotelDetailsText">
+                  <h1 className="hotelTitle">{data.title}</h1>
+                  <p className="hotelDesc">{data.desc}</p>
+                </div>
+                <div className="hotelDetailsPrice">
+                  <h1>Perfect for a 9-night stay!</h1>
+                  <span>
+                    Located in the real heart of Krakow, this property has an
+                    excellent location score of 9.8!
+                  </span>
+                  <h2>
+                    <b>$945</b> (9 nights)
+                  </h2>
+                  <button>Reserve or Book Now!</button>
+                </div>
+              </div>
             </div>
+            <MailList />
+            <Footer />
           </div>
-        </div>
-        <MailList />
-        <Footer />
-      </div>
+        </>
+      )}{" "}
     </div>
   );
 };
